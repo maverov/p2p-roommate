e41667659a70974ed65ce2de582b982a531623ff
Chuuch <daniel.chuchulev96@gmail.com>
2026-06-12 20:47:04 +0300

    Enable configuration properties for JWT in P2pRoommateApplication

--- apps/api/src/main/java/com/roommate/p2p_roommate/P2pRoommateApplication.java
+++ apps/api/src/main/java/com/roommate/p2p_roommate/P2pRoommateApplication.java
@@ -2,8 +2,12 @@ package com.roommate.p2p_roommate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.roommate.p2p_roommate.global.security.JwtProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class P2pRoommateApplication {
 
 	public static void main(String[] args) {
